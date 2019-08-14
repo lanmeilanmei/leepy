0e1e27f1df7d6898a1df525d345e461c9408d86d
zhang.kun <zhang.kun@eisoo.com>
2019-08-14 15:56:16 +0800

    basic templates for tree with some exercise a

---
+++ trees/LC100.py
@@ -0,0 +1,50 @@
+"""
+100. Same Tree
+
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+"""
+# notes: 43/67
+
+
+class SolutionT100(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q: return True
+        if not p or not q: return False
+        left = self.isSameTree(p.left, q.left)
+        right = self.isSameTree(p.right, q.right)
+        return p.val == q.val and left and right
