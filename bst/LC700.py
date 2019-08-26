7d69c029a78d7246c471f905214f19db1ec0fd90
zhang.kun <zhang.kun@eisoo.com>
2019-08-26 13:35:03 +0800

    Search in a Binary Search Tree

---
+++ bst/LC700.py
@@ -0,0 +1,20 @@
+"""
+700. Search in a Binary Search Tree
+BST查找
+"""
+# 树的遍历方式, 见trees模块
+
+
+class SolutionT700(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root: return None
+        if root.val < val:
+            return self.searchBST(root.right, val)
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        return root
