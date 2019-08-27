79f9d8ac4ffbd62da3008302be817b2978028de6
lanmeilanmei <zonkis2016@outlook.com>
2019-08-27 16:42:03 +0800

    binary search example

--- binary_search/Examples.py
+++ binary_search/Examples.py
@@ -6,13 +6,37 @@
 class SolutionEx(object):
     def binary_search_01(self, A, val, left, right):
         """
-        返回给定值在有序数组中的index,数组元素唯一。未找到则返回-1
+        返回给定值在有序数组中的index,数组元素唯一。未找到则返回-1  [要求已排序]
         """
         while left < right:
             mid = left + (right - left) // 2
-            if A[mid] == val: return mid
-            if A[mid] > val:
+            if A[mid] == val: return mid        # f(m)
+            if A[mid] > val:                    # g(m)
                 right = mid
             else:
                 left = mid + 1
         return -1
+
+    def lower_bound(self, A, val, left, right):
+        """
+        返回某数在有序数组中的下界index  [要求已排序]
+        """
+        while left < right:
+            mid = left + (right - left) // 2
+            if A[mid] >= val:                   # g(m), 因为f(m)不一定有
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+    def upper_bound(self, A, val, left, right):
+        """
+        返回某数在有序数组中的上界index  [要求已排序]
+        """
+        while left < right:
+            mid = left + (right - left) // 2
+            if A[mid] > val:
+                right = mid
+            else:
+                left = mid + 1
+        return left
--- test/binary_search/binary_search_test.py
+++ test/binary_search/binary_search_test.py
@@ -5,5 +5,12 @@ from test.binary_search.binary_search_data import BinarySearchData
 class BinarySearchTest(unittest.TestCase):
     def test_examples(self):
         from binary_search.Examples import SolutionEx
-        self.assertEqual(SolutionEx().binary_search_01(BinarySearchData.A, 8, 0, 6), 4)
-        # self.assertEqual(SolutionEx().binary_search_01(BinarySearchData.A, 6, 0, 6), -1)
+        self.assertEqual(SolutionEx().binary_search_01(BinarySearchData.A1, 8, 0, 6), 4)
+        self.assertEqual(SolutionEx().binary_search_01(BinarySearchData.A1, 6, 0, 6), -1)
+
+        self.assertEqual(SolutionEx().lower_bound(BinarySearchData.A2, 2, 0, 7), 1)
+        self.assertEqual(SolutionEx().lower_bound(BinarySearchData.A2, 3, 0, 7), 4)
+        self.assertEqual(SolutionEx().lower_bound(BinarySearchData.A3, 3, 0, 5), 2)
+
+        self.assertEqual(SolutionEx().upper_bound(BinarySearchData.A2, 5, 0, 7), 7)
+        self.assertEqual(SolutionEx().upper_bound(BinarySearchData.A2, 2, 0, 7), 4)
