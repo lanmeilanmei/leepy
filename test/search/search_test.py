beb4ab7ef8674b22cce556b526b7ee02016fcd22
lanmeilanmei <zonkis2016@outlook.com>
2019-09-16 23:55:52 +0800

    unresolved

--- test/search/search_test.py
+++ test/search/search_test.py
@@ -59,3 +59,9 @@ class SearchTest(unittest.TestCase):
         from search.LC678 import Solution
         self.assertEqual(Solution().checkValidString("(*))"), True)
         self.assertEqual(Solution().checkValidString("(**())"), True)
+
+    def test_lc491(self):
+        from search.LC491 import Solution
+        self.assertEqual(Solution().findSubsequences([4, 6, 7, 7]), [[4, 6], [4, 6, 7], [4, 6, 7, 7], [4, 7], [4, 7, 7], [6, 7], [6, 7, 7], [7, 7]])
+        # self.assertEqual(Solution().findSubsequences([4, 3, 2, 1]), [])
+        # self.assertEqual(Solution().bfs([4, 6, 7, 7]), [])
