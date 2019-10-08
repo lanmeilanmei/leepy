61b861e0f529684ba98659cf911151765be39d1b
lanmeilanmei <zonkis2016@outlook.com>
2019-10-08 16:52:50 +0800

    数独, 回溯法, official, ⭐

--- search/LC37.py
+++ search/LC37.py
@@ -16,9 +16,10 @@ Note:
 你可以假设给定的数独只有唯一解。
 给定数独永远是 9x9 形式的。
 """
+# 官方代码, https://leetcode-cn.com/problems/sudoku-solver/solution/jie-shu-du-by-leetcode/
 
 
-# unresolved
+# official
 class Solution(object):
     def solveSudoku(self, board):
         """
@@ -65,6 +66,69 @@ class Solution(object):
                 self.cols[x][i] = 0
                 self.rows[y][i] = 0
         return False
+
+    def solveSudoku_official(self, board):
+        from collections import defaultdict
+
+        def could_place(d, row, col):
+            """ 检查是否能够在(row, col)处放置数字d, 需要检查行、列、块 """
+            return not (d in rows[row] or d in columns[col] or d in boxes[box_index(row, col)])
+
+        def place_number(d, row, col):
+            """ 在(row, col)处放置数字d """
+            rows[row][d] += 1
+            columns[col][d] += 1
+            boxes[box_index(row, col)][d] += 1
+            board[row][col] = str(d)
+
+        def remove_number(d, row, col):
+            """ 移除不符合条件的数字d """
+            del rows[row][d]
+            del columns[col][d]
+            del boxes[box_index(row, col)][d]
+            board[row][col] = "."
+
+        def place_next_number(row, col):
+            """ 递归调用backtrack函数, 继续放置数字直至有解 """
+            # 遍历完最后一个位置, 有解
+            if col == N - 1 and row == N - 1:
+                nonlocal sudoku_solved
+                sudoku_solved = True
+            else:
+                if col == N - 1:    # 当前列位置遍历完, 则行数进位
+                    backtrack(row + 1, 0)
+                else:
+                    backtrack(row, col+1)
+
+        def backtrack(row=0, col=0):
+            if board[row][col] == ".":
+                for dd in range(1, 10):
+                    if could_place(dd, row, col):
+                        place_number(dd, row, col)
+                        place_next_number(row, col)
+
+                        if not sudoku_solved:
+                            remove_number(dd, row, col)
+            else:
+                place_next_number(row, col)
+
+        n = 3
+        N = n * n
+        box_index = lambda row, col: (row // n) * n + col // n # 计算块的索引匿名函数
+
+        rows = [defaultdict(int) for i in range(N)]
+        columns = [defaultdict(int) for i in range(N)]
+        boxes = [defaultdict(int) for i in range(N)]
+
+        # 记录初始九宫格状态
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] != ".":
+                    d = int(board[i][j])
+                    place_number(d, i, j)
+
+        sudoku_solved = False
+        backtrack()
 
 
 if __name__ == "__main__":
