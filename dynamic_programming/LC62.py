2f7dc61ecb894df5aa49f5e27061ca18033618d7
lanmeilanmei <zonkis2016@outlook.com>
2019-11-06 23:27:58 +0800

    不同路径 DP unfinished ⭐

---
+++ dynamic_programming/LC62.py
@@ -0,0 +1,58 @@
+"""
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+
+问总共有多少条不同的路径？
+
+说明：m 和 n 的值均不超过 100。
+
+示例 1:
+
+输入: m = 3, n = 2
+输出: 3
+解释:
+从左上角开始，总共有 3 条路径可以到达右下角。
+1. 向右 -> 向右 -> 向下
+2. 向右 -> 向下 -> 向右
+3. 向下 -> 向右 -> 向右
+"""
+# unfinished O(m*n) & O(m*n) => O(m*n) & O(2*n) => O(m*n) & O(n)
+
+
+import math
+
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        return int(math.factorial(m + n - 2) / math.factorial(m - 1) / math.factorial(n - 1))
+
+    def uniquePaths_2(self, m, n):
+        dp = [[1] * n] + [[1] + [0] * (n - 1) for _ in range(m - 1)]  # 保证最左侧和最上侧为 1
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[-1][-1]
+
+    def uniquePaths_3(self, m, n):
+        pre = [1] * n
+        cur = [1] * n
+
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] = pre[j] + cur[j - 1]
+            pre = cur[:]
+        return pre[-1]
+
+    def uniquePaths_4(self, m: int, n: int) -> int:
+        cur = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] += cur[j - 1]
+        return cur[-1]
