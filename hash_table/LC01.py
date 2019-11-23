ced79a7306a71b7f868dda3cfcbc906648e3b26f
lanmeilanmei <zonkis2016@outlook.com>
2019-11-23 18:35:03 +0800

    两数之和 hash table

---
+++ hash_table/LC01.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+
+if __name__ == '__main__':
+    print(Solution().twoSum([-3, 4, 3, 90], 0))
+    pass
