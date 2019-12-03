dec83c0994f91c2307b51cd52eff6dd680d3ed4a
lanmeilanmei <zonkis2016@outlook.com>
2019-12-03 17:23:42 +0800

    计算右侧大于当前数字的个数  分治法 unresolved

---
+++ divide_and_conquer/LC315.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+
+
+class Solution(object):
+    def countSmaller(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        another = [nums[-1]]
+        res = [0]
+
+
